0d3c004b1598c9fb2e90cb04297b6630cbc03939
spicyjpeg <thatspicyjpeg@gmail.com>
2024-12-30 13:30:53 +0100

    Reduce audio asset size, update JSON schemas

--- assets/sounds/convertSounds.sh
+++ assets/sounds/convertSounds.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ROOT_DIR="$(dirname "$0")"
-ENCODE_VAG="psxavenc -q -t vag -a 64 -c 1"
-ENCODE_MP3="lame -S -q 0 -b 96 -t --noreplaygain"
+ENCODE_VAG="psxavenc -q -t vag -a 64 -f 22050 -c 1"
+ENCODE_MP3="lame -S -q 0 -b 96 -t --resample 44100 --noreplaygain"
 
 if ! which psxavenc lame >/dev/null 2>&1; then
 	echo \
@@ -11,14 +11,14 @@ if ! which psxavenc lame >/dev/null 2>&1; then
 	exit 1
 fi
 
-$ENCODE_VAG -f 22050 -L "$ROOT_DIR/about.wav"      "$ROOT_DIR/about.vag"
-$ENCODE_VAG -f 22050    "$ROOT_DIR/alert.wav"      "$ROOT_DIR/alert.vag"
-$ENCODE_VAG -f 22050    "$ROOT_DIR/click.wav"      "$ROOT_DIR/click.vag"
-$ENCODE_VAG -f 22050    "$ROOT_DIR/enter.wav"      "$ROOT_DIR/enter.vag"
-$ENCODE_VAG -f 22050    "$ROOT_DIR/exit.wav"       "$ROOT_DIR/exit.vag"
-$ENCODE_VAG -f 22050    "$ROOT_DIR/move.wav"       "$ROOT_DIR/move.vag"
-$ENCODE_VAG -f 33075    "$ROOT_DIR/screenshot.wav" "$ROOT_DIR/screenshot.vag"
-$ENCODE_VAG -f 33075    "$ROOT_DIR/startup.wav"    "$ROOT_DIR/startup.vag"
+$ENCODE_VAG -L "$ROOT_DIR/about.wav"      "$ROOT_DIR/about.vag"
+$ENCODE_VAG    "$ROOT_DIR/alert.wav"      "$ROOT_DIR/alert.vag"
+$ENCODE_VAG    "$ROOT_DIR/click.wav"      "$ROOT_DIR/click.vag"
+$ENCODE_VAG    "$ROOT_DIR/enter.wav"      "$ROOT_DIR/enter.vag"
+$ENCODE_VAG    "$ROOT_DIR/exit.wav"       "$ROOT_DIR/exit.vag"
+$ENCODE_VAG    "$ROOT_DIR/move.wav"       "$ROOT_DIR/move.vag"
+$ENCODE_VAG    "$ROOT_DIR/screenshot.wav" "$ROOT_DIR/screenshot.vag"
+$ENCODE_VAG    "$ROOT_DIR/startup.wav"    "$ROOT_DIR/startup.vag"
 
 # This file is used by the digital I/O board MP3 playback test.
-$ENCODE_MP3 --resample 44100 "$ROOT_DIR/startup.wav" "$ROOT_DIR/startup.mp3"
+$ENCODE_MP3 "$ROOT_DIR/startup.wav" "$ROOT_DIR/startup.mp3"
